3b1131187e9607a305f061ac6b1dcd74e9311ade
like5188 <260864866@qq.com>
2017-07-07 17:07:47 +0800

    pieCharView

--- sample/src/main/java/com/like/common/sample/PieChartActivity.kt
+++ sample/src/main/java/com/like/common/sample/PieChartActivity.kt
@@ -19,24 +19,31 @@ class PieChartActivity : BaseActivity() {
     }
 
     fun changeData1(view: View) {
-        mBinding.pieView.setData(getSimulatedData1())
+        val pieData = getSimulatedData1()
+        mBinding.pieData = pieData
+        mBinding.pieView.setData(pieData)
     }
 
     fun changeData2(view: View) {
-        mBinding.pieView.setData(getSimulatedData2())
+        val pieData = getSimulatedData2()
+        mBinding.pieData = pieData
+        mBinding.pieView.setData(pieData)
     }
 
     fun changeData3(view: View) {
-        mBinding.pieView.setData(getSimulatedData3())
+        val pieData = getSimulatedData3()
+        mBinding.pieData = pieData
+        mBinding.pieView.setData(pieData)
     }
 
     fun clearData(view: View) {
+        mBinding.pieData = null
         mBinding.pieView.setData(null)
     }
 
-    fun getSimulatedData1(): PieData = PieData(2017, 1, listOf(MonthData(1, 100f, 100f), MonthData(2, 200f, 200f), MonthData(3, 300f, 300f)))
+    fun getSimulatedData1(): PieData = PieData(2015, 1, listOf(MonthData(1, 100f, 100f), MonthData(2, 200f, 200f), MonthData(3, 300f, 300f)))
 
-    fun getSimulatedData2(): PieData = PieData(2017, 2, listOf(MonthData(4, 400f, 400f), MonthData(5, 400f, 400f), MonthData(6, 400f, 400f)))
+    fun getSimulatedData2(): PieData = PieData(2016, 2, listOf(MonthData(4, 400f, 400f), MonthData(5, 400f, 400f), MonthData(6, 400f, 400f)))
 
     fun getSimulatedData3(): PieData = PieData(2017, 3, listOf(MonthData(7, 700f, 700f), MonthData(8, 1400f, 1400f), MonthData(9, 700f, 700f)))
 }
