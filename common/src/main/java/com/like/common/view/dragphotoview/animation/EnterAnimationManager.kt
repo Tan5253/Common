7f242cbed1cebcf491e670c3a326d1f74eb73255
like5188 <260864866@qq.com>
2017-09-23 22:04:12 +0800

    DragPhotoView自定义

--- common/src/main/java/com/like/common/view/dragphotoview/animation/EnterAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragphotoview/animation/EnterAnimationManager.kt
@@ -1,7 +1,7 @@
 package com.like.common.view.dragphotoview.animation
 
 import android.animation.AnimatorSet
-import android.animation.ValueAnimator
+import android.animation.ObjectAnimator
 import com.like.common.view.dragphotoview.DragPhotoView
 import com.like.common.view.dragphotoview.DragPhotoViewInfo
 
@@ -10,38 +10,18 @@ import com.like.common.view.dragphotoview.DragPhotoViewInfo
  */
 class EnterAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: DragPhotoViewInfo) : AnimationManager(dragPhotoView, dragPhotoViewInfo) {
     init {
-        dragPhotoView.scaleX = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.mWidth
-        dragPhotoView.scaleY = dragPhotoViewInfo.originHeight.toFloat() / dragPhotoView.mHeight
+        dragPhotoView.scaleX = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.width
+        dragPhotoView.scaleY = dragPhotoViewInfo.originHeight.toFloat() / dragPhotoView.height
 
-        dragPhotoView.translationX = dragPhotoViewInfo.originCenterX - dragPhotoView.mWidth / 2
-        dragPhotoView.translationY = dragPhotoViewInfo.originCenterY - dragPhotoView.mHeight / 2
+        dragPhotoView.translationX = dragPhotoViewInfo.originCenterX - dragPhotoView.width.toFloat() / 2
+        dragPhotoView.translationY = dragPhotoViewInfo.originCenterY - dragPhotoView.height.toFloat() / 2
     }
 
     override fun fillAnimatorSet(animatorSet: AnimatorSet) {
-        animatorSet.play(ValueAnimator.ofFloat(dragPhotoView.x, 0f).apply {
-            duration = AnimationManager.DURATION
-            addUpdateListener {
-                dragPhotoView.x = it.animatedValue as Float
-            }
-        })
-                .with(ValueAnimator.ofFloat(dragPhotoView.y, 0f).apply {
-                    duration = AnimationManager.DURATION
-                    addUpdateListener {
-                        dragPhotoView.y = it.animatedValue as Float
-                    }
-                })
-                .with(ValueAnimator.ofFloat(dragPhotoView.scaleX, 1f).apply {
-                    duration = AnimationManager.DURATION
-                    addUpdateListener {
-                        dragPhotoView.scaleX = it.animatedValue as Float
-                    }
-                })
-                .with(ValueAnimator.ofFloat(dragPhotoView.scaleY, 1f).apply {
-                    duration = AnimationManager.DURATION
-                    addUpdateListener {
-                        dragPhotoView.scaleY = it.animatedValue as Float
-                    }
-                })
+        animatorSet.play(ObjectAnimator.ofFloat(dragPhotoView, "x", dragPhotoView.x, 0f))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "y", dragPhotoView.y, 0f))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleX", dragPhotoView.scaleX, 1f))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleY", dragPhotoView.scaleY, 1f))
     }
 
 }
