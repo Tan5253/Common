e68fa55c0d9bd55f4af02a649a463fff70589621
like5188 <260864866@qq.com>
2017-09-23 22:27:54 +0800

    DragPhotoView自定义

--- common/src/main/java/com/like/common/view/dragphotoview/animation/EnterAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragphotoview/animation/EnterAnimationManager.kt
@@ -9,19 +9,24 @@ import com.like.common.view.dragphotoview.DragPhotoViewInfo
  * 进入DragPhotoViewActivity的动画
  */
 class EnterAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: DragPhotoViewInfo) : AnimationManager(dragPhotoView, dragPhotoViewInfo) {
-    init {
-        dragPhotoView.scaleX = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.width
-        dragPhotoView.scaleY = dragPhotoViewInfo.originHeight.toFloat() / dragPhotoView.height
+    private val initScaleX = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.width
+    private val initScaleY = dragPhotoViewInfo.originHeight.toFloat() / dragPhotoView.height
+    private val initTranslationX = dragPhotoViewInfo.originCenterX - dragPhotoView.width.toFloat() / 2
+    private val initTranslationY = dragPhotoViewInfo.originCenterY - dragPhotoView.height.toFloat() / 2
 
-        dragPhotoView.translationX = dragPhotoViewInfo.originCenterX - dragPhotoView.width.toFloat() / 2
-        dragPhotoView.translationY = dragPhotoViewInfo.originCenterY - dragPhotoView.height.toFloat() / 2
+    init {
+        dragPhotoView.scaleX = initScaleX
+        dragPhotoView.scaleY = initScaleY
+
+        dragPhotoView.translationX = initTranslationX
+        dragPhotoView.translationY = initTranslationY
     }
 
     override fun fillAnimatorSet(animatorSet: AnimatorSet) {
         animatorSet.play(ObjectAnimator.ofFloat(dragPhotoView, "x", dragPhotoView.x, 0f))
                 .with(ObjectAnimator.ofFloat(dragPhotoView, "y", dragPhotoView.y, 0f))
-                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleX", dragPhotoView.scaleX, 1f))
-                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleY", dragPhotoView.scaleY, 1f))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleX", initScaleX, 1f))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleY", initScaleY, 1f))
     }
 
 }
