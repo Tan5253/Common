57bc1561f77d5ea2c70aa5fd8059ec81f086d044
like5188 <260864866@qq.com>
2017-09-25 10:44:12 +0800

    增加了自定义的DragPhotoView

--- common/src/main/java/com/like/common/view/dragphotoview/animation/ExitAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragphotoview/animation/ExitAnimationManager.kt
@@ -10,25 +10,29 @@ import com.like.common.view.dragphotoview.DragPhotoViewInfo
  * 从缩放状态退出DragPhotoViewActivity的动画
  */
 class ExitAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: DragPhotoViewInfo) : AnimationManager(dragPhotoView, dragPhotoViewInfo) {
+    val halfDragPhotoViewWidth = dragPhotoView.width / 2
+    val halfDragPhotoViewHeight = dragPhotoView.height / 2
+    val halfScaleDragPhotoViewWidth = dragPhotoView.width * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
+    val halfScaleDragPhotoViewHeight = dragPhotoView.height * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
     private var pendingTranslateX = 0f
     private var pendingTranslateY = 0f
 
     fun setData(curTranslationX: Float, curTranslationY: Float): ExitAnimationManager {
-        // 把缩放后的dragPhotoView移动到初始位置，并刷新。这一步是为了解决拖拽时有可能导致dragPhotoView显示的图片不完整（被屏幕边缘剪切了）。
+        // 把缩放后的dragPhotoView移动到屏幕左上角的位置，这样能保证不管是否缩放，都能显示完整的图片，并刷新。这一步是为了解决拖拽时有可能导致dragPhotoView显示的图片不完整（被屏幕边缘剪切了）。
         // 如果不做处理，只需要下面的代码
-//        val newViewX = dragPhotoView.width / 2 + x - dragPhotoView.width * mScaleX / 2
-//        val newViewY = dragPhotoView.height / 2 + y - dragPhotoView.height * mScaleY / 2
+//        val newViewX = halfDragPhotoViewWidth + curTranslationX - halfScaleDragPhotoViewWidth
+//        val newViewY = halfDragPhotoViewHeight + curTranslationY - halfScaleDragPhotoViewHeight
 //        val translateXAnimator = ValueAnimator.ofFloat(0f, dragPhotoViewInfo.originLeft.toFloat() - newViewX)
 //        val translateYAnimator = ValueAnimator.ofFloat(0f, dragPhotoViewInfo.originTop.toFloat() - newViewY)
-        dragPhotoView.mRestoreAnimationManager.canvasTranslationX = -dragPhotoView.width / 2 + dragPhotoView.width * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
-        dragPhotoView.mRestoreAnimationManager.canvasTranslationY = -dragPhotoView.height / 2 + dragPhotoView.height * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
+        dragPhotoView.mRestoreAnimationManager.canvasTranslationX = -halfDragPhotoViewWidth + halfScaleDragPhotoViewWidth
+        dragPhotoView.mRestoreAnimationManager.canvasTranslationY = -halfDragPhotoViewHeight + halfScaleDragPhotoViewHeight
         dragPhotoView.invalidate()
         // 把缩放后的dragPhotoView移动到手指释放时的位置，准备开始动画。
-        dragPhotoView.x = dragPhotoView.mWidth / 2 + curTranslationX - dragPhotoView.mWidth * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
-        dragPhotoView.y = dragPhotoView.mHeight / 2 + curTranslationY - dragPhotoView.mHeight * dragPhotoView.mRestoreAnimationManager.canvasScale / 2
-        // 计算缩放后的dragPhotoView和原始的dragPhotoView的位移
-        val curCenterX = dragPhotoView.x + dragPhotoViewInfo.originWidth / 2
-        val curCenterY = dragPhotoView.y + dragPhotoViewInfo.originHeight / 2
+        dragPhotoView.x = halfDragPhotoViewWidth + curTranslationX - halfScaleDragPhotoViewWidth
+        dragPhotoView.y = halfDragPhotoViewHeight + curTranslationY - halfScaleDragPhotoViewHeight
+        // 计算缩放后的dragPhotoView距离原始的dragPhotoView的位移
+        val curCenterX = dragPhotoView.x + halfScaleDragPhotoViewWidth
+        val curCenterY = dragPhotoView.y + halfScaleDragPhotoViewHeight
         pendingTranslateX = dragPhotoViewInfo.originCenterX - curCenterX
         pendingTranslateY = dragPhotoViewInfo.originCenterY - curCenterY
         return this
