c6a1471611192b80cff10e7ad71c1017d10affc7
like5188 <260864866@qq.com>
2017-09-26 11:19:56 +0800

    增加了自定义的DragPhotoView

--- common/src/main/java/com/like/common/view/dragphotoview/animation/RestoreAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragphotoview/animation/RestoreAnimationManager.kt
@@ -4,10 +4,8 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ValueAnimator
-import android.util.TypedValue
 import com.like.common.view.dragphotoview.DragPhotoView
 import com.like.common.view.dragphotoview.DragPhotoViewInfo
-import com.like.logger.Logger
 
 /**
  * 从缩放状态在DragPhotoViewActivity中还原的动画管理
@@ -17,8 +15,8 @@ class RestoreAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: D
     var canvasTranslationX: Float = 0f
     var canvasTranslationY: Float = 0f
     var canvasScale: Float = 1f
-    private val MIN_CANVAS_SCALE: Float = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.width
-    val MAX_CANVAS_TRANSLATION_Y: Float = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100f, dragPhotoView.resources.displayMetrics)
+    val MIN_CANVAS_SCALE: Float = dragPhotoViewInfo.originWidth.toFloat() / dragPhotoView.width
+    val MAX_CANVAS_TRANSLATION_Y: Float = dragPhotoView.height.toFloat() / 4
 
     override
     fun fillAnimatorSet(animatorSet: AnimatorSet) {
@@ -64,8 +62,7 @@ class RestoreAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: D
     }
 
     fun updateCanvasScale() {
-        Logger.e("canvasTranslationY = $canvasTranslationY MAX_CANVAS_TRANSLATION_Y = $MAX_CANVAS_TRANSLATION_Y")
-        val translateYPercent = canvasTranslationY / MAX_CANVAS_TRANSLATION_Y
+        val translateYPercent = canvasTranslationY / dragPhotoView.height
         val scale = 1 - translateYPercent
         canvasScale = when {
             scale < MIN_CANVAS_SCALE -> MIN_CANVAS_SCALE
@@ -75,8 +72,7 @@ class RestoreAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: D
     }
 
     fun updateCanvasBgAlpha() {
-        Logger.e("canvasTranslationY = $canvasTranslationY MAX_CANVAS_TRANSLATION_Y = $MAX_CANVAS_TRANSLATION_Y")
-        val translateYPercent = canvasTranslationY / MAX_CANVAS_TRANSLATION_Y
+        val translateYPercent = canvasTranslationY / dragPhotoView.height
         val alpha = (255 * (1 - translateYPercent)).toInt()
         canvasBgAlpha = when {
             alpha > 255 -> 255
