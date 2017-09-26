4cef2937a940ed799dbcbb6f988a1cd9a320ad59
like5188 <260864866@qq.com>
2017-09-26 17:28:52 +0800

    增加了自定义的DragPhotoView

--- common/src/main/java/com/like/common/view/dragphotoview/animation/DisappearAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragphotoview/animation/DisappearAnimationManager.kt
@@ -11,14 +11,14 @@ import com.like.common.view.dragphotoview.DragPhotoViewInfo
  */
 class DisappearAnimationManager(dragPhotoView: DragPhotoView, dragPhotoViewInfo: DragPhotoViewInfo) : AnimationManager(dragPhotoView, dragPhotoViewInfo) {
     private val initTranslationX = dragPhotoView.width.toFloat() * dragPhotoViewInfo.index// 要考虑ViewPager切换页面对初始值的影响
-    private val pendingTranslationX = dragPhotoViewInfo.originCenterX - dragPhotoView.width.toFloat() / 2
-    private val pendingTranslationY = dragPhotoViewInfo.originCenterY - dragPhotoView.height.toFloat() / 2
     private val pendingScaleX = dragPhotoViewInfo.originWidth / dragPhotoView.width.toFloat()
     private val pendingScaleY = dragPhotoViewInfo.originHeight / dragPhotoView.height.toFloat()
 
     override fun fillAnimatorSet(animatorSet: AnimatorSet) {
-        animatorSet.play(ObjectAnimator.ofFloat(dragPhotoView, "x", initTranslationX, initTranslationX + pendingTranslationX))
-                .with(ObjectAnimator.ofFloat(dragPhotoView, "y", 0f, pendingTranslationY))
+        dragPhotoView.pivotX = 0f
+        dragPhotoView.pivotY = 0f
+        animatorSet.play(ObjectAnimator.ofFloat(dragPhotoView, "x", initTranslationX, initTranslationX + dragPhotoViewInfo.originLeft.toFloat()))
+                .with(ObjectAnimator.ofFloat(dragPhotoView, "y", 0f, dragPhotoViewInfo.originTop.toFloat()))
                 .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleX", 1f, pendingScaleX))
                 .with(ObjectAnimator.ofFloat(dragPhotoView, "scaleY", 1f, pendingScaleY))
     }
