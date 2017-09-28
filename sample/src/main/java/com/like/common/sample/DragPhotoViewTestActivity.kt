e874bbd8ab3b773e33194a8a37d69991c5a458f0
like5188 <260864866@qq.com>
2017-09-28 09:26:50 +0800

    增加了自定义的DragVideoView

--- sample/src/main/java/com/like/common/sample/DragPhotoViewTestActivity.kt
+++ sample/src/main/java/com/like/common/sample/DragPhotoViewTestActivity.kt
@@ -24,29 +24,36 @@ class DragPhotoViewTestActivity : BaseActivity() {
     }
 
     fun onClick(view: View) {
-        val intent = Intent(this, DragPhotoViewActivity::class.java)
         val list = ArrayList<DragPhotoViewInfo>()
+        when (view.id) {
+            R.id.iv_0, R.id.iv_1, R.id.iv_2 -> {
+                /**
+                 * view.getLocationInWindow(location); //获取在当前窗口内的绝对坐标
+                 * dragPhotoView.getLocationOnScreen(location);//获取在整个屏幕内的绝对坐标
+                 * getLocationOnScreen计算该视图在全局坐标系中的x，y值，
+                 * (注意这个值是要从屏幕顶端算起，也就是索包括了通知栏的高度)//获取在当前屏幕内的绝对坐标
+                 * location [0]--->x坐标,location [1]--->y坐标
+                 *
+                 */
+                val location0 = IntArray(2)
+                mBinding.iv0.getLocationOnScreen(location0)
+                list.add(DragPhotoViewInfo(location0[0], location0[1], mBinding.iv0.width, mBinding.iv0.height, "", R.drawable.wugeng, view.id == R.id.iv_0))
 
-        /**
-         * view.getLocationInWindow(location); //获取在当前窗口内的绝对坐标
-         * dragPhotoView.getLocationOnScreen(location);//获取在整个屏幕内的绝对坐标
-         * getLocationOnScreen计算该视图在全局坐标系中的x，y值，
-         * (注意这个值是要从屏幕顶端算起，也就是索包括了通知栏的高度)//获取在当前屏幕内的绝对坐标
-         * location [0]--->x坐标,location [1]--->y坐标
-         *
-         */
-        val location0 = IntArray(2)
-        mBinding.iv0.getLocationOnScreen(location0)
-        list.add(DragPhotoViewInfo(location0[0], location0[1], mBinding.iv0.width, mBinding.iv0.height, "", R.drawable.wugeng, view.id == R.id.iv_0))
+                val location1 = IntArray(2)
+                mBinding.iv1.getLocationOnScreen(location1)
+                list.add(DragPhotoViewInfo(location1[0], location1[1], mBinding.iv1.width, mBinding.iv1.height, "", R.drawable.wugeng1, view.id == R.id.iv_1))
 
-        val location1 = IntArray(2)
-        mBinding.iv1.getLocationOnScreen(location1)
-        list.add(DragPhotoViewInfo(location1[0], location1[1], mBinding.iv1.width, mBinding.iv1.height, "", R.drawable.wugeng1, view.id == R.id.iv_1))
-
-        val location2 = IntArray(2)
-        mBinding.iv2.getLocationOnScreen(location2)
-        list.add(DragPhotoViewInfo(location2[0], location2[1], mBinding.iv2.width, mBinding.iv2.height, "", R.drawable.wugeng2, view.id == R.id.iv_2))
-
+                val location2 = IntArray(2)
+                mBinding.iv2.getLocationOnScreen(location2)
+                list.add(DragPhotoViewInfo(location2[0], location2[1], mBinding.iv2.width, mBinding.iv2.height, "", R.drawable.wugeng2, view.id == R.id.iv_2))
+            }
+            R.id.rl_video -> {
+                val location0 = IntArray(2)
+                mBinding.rlVideo.getLocationOnScreen(location0)
+                list.add(DragPhotoViewInfo(location0[0], location0[1], mBinding.rlVideo.width, mBinding.rlVideo.height, "", R.drawable.wugeng, view.id == R.id.rl_video))
+            }
+        }
+        val intent = Intent(this, DragPhotoViewActivity::class.java)
         intent.putParcelableArrayListExtra(DragPhotoViewActivity.KEY_DATA, list)
         startActivity(intent)
         overridePendingTransition(0, 0)
