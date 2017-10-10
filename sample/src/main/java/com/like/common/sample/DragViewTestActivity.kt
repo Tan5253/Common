607f6af183fade8bfce6184ea4561011ef6812d8
like5188 <260864866@qq.com>
2017-10-10 16:09:12 +0800

    dragview glide升级到v4

--- sample/src/main/java/com/like/common/sample/DragViewTestActivity.kt
+++ sample/src/main/java/com/like/common/sample/DragViewTestActivity.kt
@@ -9,28 +9,30 @@ import com.like.base.viewmodel.BaseViewModel
 import com.like.common.sample.databinding.ActivityDragphotoviewBinding
 import com.like.common.util.GlideUtils
 import com.like.common.util.ResourceUtils
+import com.like.common.util.StorageUtils
 import com.like.common.view.dragview.entity.DragInfo
 import com.like.common.view.dragview.view.DragViewActivity
+import java.io.File
 import java.util.*
 
 class DragViewTestActivity : BaseActivity() {
-//    private val originImageUrl0: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_0_origin.jpg" }
-//    private val originImageUrl1: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_1_origin.jpg" }
-//    private val originImageUrl2: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_2_origin.jpg" }
-//    private val imageUrl0: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_0.jpg" }
-//    private val imageUrl1: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_1.jpg" }
-//    private val imageUrl2: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_2.jpg" }
-//    private val videoUrl: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/video_1.mp4" }
-//    private val videoImageUrl: String by lazy { imageUrl0 }
+    private val originImageUrl0: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_0_origin.jpg" }
+    private val originImageUrl1: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_1_origin.jpg" }
+    private val originImageUrl2: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_2_origin.jpg" }
+    private val imageUrl0: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_0.jpg" }
+    private val imageUrl1: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_1.jpg" }
+    private val imageUrl2: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}/image_2.jpg" }
+    private val videoUrl: String by lazy { "${StorageUtils.InternalStorageHelper.getBaseDir(this)}${File.separator}Video/video_1.mp4" }
+    private val videoImageUrl: String by lazy { imageUrl0 }
 
-    private val originImageUrl0: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183844md4d33a5d8d134w4.jpg.thumb.jpg" }
-    private val originImageUrl1: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183837avftmmzmfi76tkis.jpg.thumb.jpg" }
-    private val originImageUrl2: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183832tqu8fyy6qqk8a8f0.jpg.thumb.jpg" }
-    private val imageUrl0: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649503&di=f17e048c5bca2efc2879dc8e578a73f1&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D03fdfcc6d743ad4ba62e46c0b2035a89%2F8ad4b31c8701a18b5d18132c982f07082938fee6.jpg" }
-    private val imageUrl1: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=f6f24a7a3b395e1107b8ddceb3efe2bb&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F4034970a304e251f89ac4497a186c9177f3e5301.jpg" }
-    private val imageUrl2: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=112f816b18998d14223ab58269f5c057&imgtype=0&src=http%3A%2F%2Fupload.gezila.com%2Fdata%2F20150906%2F28201441522774.jpg" }
-    private val videoUrl: String by lazy { "http://he.yinyuetai.com/uploads/videos/common/31BA015D2B8D04657E61B4BF0B448B79.mp4" }
-    private val videoImageUrl: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=67866dc8e157120fb44487329a18c3c9&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fexp%2Fw%3D480%2Fsign%3D788267e49c2f07085f052b08d925b865%2F9922720e0cf3d7ca849f4addf01fbe096b63a934.jpg" }
+//    private val originImageUrl0: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183844md4d33a5d8d134w4.jpg.thumb.jpg" }
+//    private val originImageUrl1: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183837avftmmzmfi76tkis.jpg.thumb.jpg" }
+//    private val originImageUrl2: String by lazy { "http://bbsfiles.vivo.com.cn/vivobbs/attachment/forum/201601/11/183832tqu8fyy6qqk8a8f0.jpg.thumb.jpg" }
+//    private val imageUrl0: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649503&di=f17e048c5bca2efc2879dc8e578a73f1&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D03fdfcc6d743ad4ba62e46c0b2035a89%2F8ad4b31c8701a18b5d18132c982f07082938fee6.jpg" }
+//    private val imageUrl1: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=f6f24a7a3b395e1107b8ddceb3efe2bb&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F4034970a304e251f89ac4497a186c9177f3e5301.jpg" }
+//    private val imageUrl2: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=112f816b18998d14223ab58269f5c057&imgtype=0&src=http%3A%2F%2Fupload.gezila.com%2Fdata%2F20150906%2F28201441522774.jpg" }
+//    private val videoUrl: String by lazy { "http://he.yinyuetai.com/uploads/videos/common/31BA015D2B8D04657E61B4BF0B448B79.mp4" }
+//    private val videoImageUrl: String by lazy { "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507451649502&di=67866dc8e157120fb44487329a18c3c9&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fexp%2Fw%3D480%2Fsign%3D788267e49c2f07085f052b08d925b865%2F9922720e0cf3d7ca849f4addf01fbe096b63a934.jpg" }
 
     private val mBinding: ActivityDragphotoviewBinding by lazy {
         DataBindingUtil.setContentView<ActivityDragphotoviewBinding>(this, R.layout.activity_dragphotoview)
