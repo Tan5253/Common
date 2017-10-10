1803968469e28d230e1c503d9684c3407b1467b4
like5188 <260864866@qq.com>
2017-10-10 10:29:57 +0800

    dragview glide升级到v4

--- common/src/main/java/com/like/common/util/CustomGlideModule.java
+++ common/src/main/java/com/like/common/util/CustomGlideModule.java
@@ -3,6 +3,9 @@ package com.like.common.util;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+/**
+ * Glide V4 Generated API 需要的
+ */
 @GlideModule
 public class CustomGlideModule extends AppGlideModule {
 }
